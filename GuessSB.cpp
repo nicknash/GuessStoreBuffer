8cf5e524c0a8aabf660c42b799da49042cf3f9eb
Nick Nash <nicholas.nash@gmail.com>
2018-03-08 07:01:02 +0000

    Switch to C++ to for a bit more control

---
+++ GuessSB.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <sys/time.h>
+
+double time_diff_millis(struct timeval start_time, struct timeval end_time)
+{
+    return 1000*(end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) / 1000.0;
+}
+
+int main()
+{
+	using namespace std;
+	long* p = new long[1024];
+/*
+	for(int i = 0; i < 1024; ++i)
+	{
+		p[i] = i;
+	}
+*/
+	long pa = (long) p;
+	long *q = (long*) (pa + (128 - (pa & 0x7F)));
+
+
+	ofstream results;
+  	results.open("timings.csv", ios::out);
+	results << "NumStores,TimeInMicroseconds" << endl;
+	
+	for (int numStores = 20; numStores < 80; ++numStores) // 42 entry store buffer on haswell.
+    {
+    	struct timeval start;
+    	gettimeofday(&start, NULL);
+		const int numTrials = 1000000;
+        for (int i = 0; i < numTrials; ++i)
+        {
+        	for (int j = 0; j < numStores; ++j)
+            {
+            	q[j] = j;
+            }
+            for (int j = 0; j < 550; ++j)
+            {
+            	asm("nop");
+            	/*if (i > 10000000)
+                {
+                	break;
+                }*/
+            }
+        }
+ 		struct timeval end;
+    	gettimeofday(&end, NULL);
+    	double micsPerIteration = time_diff_millis(start, end)/numTrials * 1e3;
+        long total = 0;
+    	for(int i = 0; i < 1024; ++i)
+    	{	
+    		total += p[i];
+    	}
+    	cout << "NumStores=" << numStores << " total = " << total << " micsPerIteration = " << micsPerIteration << endl;
+    	results << numStores << "," << micsPerIteration << endl;
+	}
+	results.flush();
+	results.close();
+	return 0;
+}
